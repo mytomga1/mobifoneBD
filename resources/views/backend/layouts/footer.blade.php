b46da0b3153d32a246b88c79659ed5ee2e26a547
ADMIN-PC\mytomga <mr.mytomga@gmail.com>
2023-09-18 15:43:30 +0700

    fist commit

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- footer    ========================================================================================================================== -->
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2023 <a href="#">MobiFone Bình Dương</a>.</strong>Design by LTTrung
+    reserved.
+</footer>
+<!-- End footer ========================================================================================================================= -->
